f4278dded65282c3162b0a0fcecc9cee43c07758
andyjDavison <andyd.soccer@gmail.com>
2023-03-14 08:16:15 -0400

    Add DataConstants

---
+++ DataConstants.java
@@ -0,0 +1,10 @@
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "json/users.JSON";
+    protected static final String USER_ID = "id";
+    protected static final String USER_TYPE = "type";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USER_NAME = "username";
+    protected static final String USER_PASSWORD = "password";
+    protected static final String USER_EMAIL = "email";
+}
