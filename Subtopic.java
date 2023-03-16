6653f36472ea3fcc0bf422761b2d07999563b9ea
Trevor Allison <trevorallison@me.com>
2023-03-16 12:47:25 -0400

    subtopic class methods filled in

--- Subtopic.java
+++ Subtopic.java
@@ -3,10 +3,16 @@ public class Subtopic {
     private String info;
 
     public Subtopic(String name, String info){
-
+        this.name = name;
+        this.info = info;
     }
+    //We will have to do some serious formatting to get this to print out a paragraph instead of a line.
+    //will fix later when we think about what format the info is in.   
     public String toString(){
-        return "";
+        String result = "";
+        result += this.name+"\n";
+        result += this.info+"\n";
+        return result;
     }
     public String getName() {
         return this.name;
