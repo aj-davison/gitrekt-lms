f096271ee5e40ad6d158fc661e37335c4e6e0678
Trevor Allison <trevorallison@me.com>
2023-03-16 16:47:58 -0400

    filling in topic class and methods except for displayGrade and toString

--- Topic.java
+++ Topic.java
@@ -2,31 +2,56 @@ import java.util.ArrayList;
 
 public class Topic {
     private ArrayList<Subtopic> subtopics;
-    private ArrayList<Comment> comments;
+    private ArrayList<Comment> comments = new ArrayList<Comment>();
     private String name;
     private Quiz quiz;
+    private Difficulty difficulty;
     private boolean complete;
+    private int position = 0;
 
     public Topic(ArrayList<Subtopic> subtopics, String name, Difficulty difficulty, Quiz quiz){
+        this.subtopics = subtopics;
+        this.name = name;
+        this.difficulty = difficulty;
+        this.quiz = quiz;
+        this.complete = false;
 
     }
     public boolean completionStatus(){
-        return true;
+        boolean result;
+        if(!hasNext()){
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
     }
+    public Subtopic nextSubtopic(){
+        if(!hasNext()){
+            return null;
+        }
+        Subtopic subtopic = subtopics.get(position);
+        return subtopic;
+    }
+    public boolean hasNext(){
+        return subtopics.get(position) != null && position < subtopics.size();
+    }
+    //TODO
     public String displayGrade(){
         return "";
     }
+    //TODO
     public String toString(){
         return "";
     }
     public void addComment(Comment comment){
-
+        comments.add(comment);
     }
     public void addQuiz(Quiz quiz){
-
+        this.quiz = quiz;
     }
     public void addSubtopic(Subtopic subtopic){
-        
+        subtopics.add(subtopic);
     }
     public ArrayList<Subtopic> getSubTop() {
         if(subtopics == null){
