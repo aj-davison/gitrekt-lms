008e56c6219be0903ba2a4ac36e461236daee5e5
Trevor Allison <trevorallison@me.com>
2023-03-16 17:00:34 -0400

    filling in comment class

--- Comment.java
+++ Comment.java
@@ -4,22 +4,35 @@ import java.util.UUID;
 public class Comment {
     private ArrayList<Comment> subComments;
     private String mainComment;
-    private UUID userID;
+    private String username;
+    private UUID id;
 
     public Comment(String mainComment, String username){
-
+        this.mainComment = mainComment;
+        this.username = username;
+        subComments = new ArrayList<>();
     }
+    public Comment(UUID id, String mainComment, String username, ArrayList<Comment> subComments){
+        this.mainComment = mainComment;
+        this.username = username;
+        this.subComments = subComments;
+        this.id = id;
+    }
+    //TODO
     public String toString(){
         return "";
     }
     public void addSubComment(Comment subComment){
-        
+        subComments.add(subComment);
     }
     public String getContent() {
-        return mainComment;
+        return this.mainComment;
     }
-    public UUID getUserID() {
-        return this.userID;
+    public String getUsername(){
+        return this.username;
+    }
+    public UUID getID() {
+        return this.id;
     }
     public ArrayList<Comment> getReplies() {
         if(subComments == null)
