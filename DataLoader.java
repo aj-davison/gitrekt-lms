88cd94e686bf7ff54f031b3e8306610143c73ba2
andyjDavison <andyd.soccer@gmail.com>
2023-03-19 09:32:13 -0400

    Add DataLoader

--- DataLoader.java
+++ DataLoader.java
@@ -28,18 +28,104 @@ public class DataLoader extends DataConstants {
                 String email = (String)userJSON.get(USER_EMAIL);
 
                 if(type.equals("student")) {
-                    users.add(new Student(null, firstName, lastName, username, password, email));
+                    users.add(new Student(id, null, firstName, lastName, email, username, password, 0, null));
                 } else {
                     users.add(new Author(id, null, usersJSON, firstName, lastName, username, password, email, i, usersJSON));
                 }
             }
-            return users;
-
+            reader.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return users;
     }
+
+    public static ArrayList<Course> getCourse() {
+        ArrayList<Course> courses = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Topic> topics = new ArrayList<>();
+        ArrayList<Subtopic> subtopics = new ArrayList<>();
+        ArrayList<Question> questions = new ArrayList<>();
+        ArrayList<Comment> comments = new ArrayList<>();
+        
+        try {
+            FileReader reader = new FileReader(COURSE_FILE_NAME);
+            JSONParser parser = new JSONParser();
+            JSONArray coursesArray = (JSONArray)parser.parse(reader);
+
+            for(int i=0;i<coursesArray.size();i++) {
+                JSONObject courseJSON = (JSONObject)coursesArray.get(i);
+                UUID id = UUID.fromString((String)courseJSON.get(COURSE_ID));
+                String title = (String)courseJSON.get(COURSE_TITLE);
+                String description = (String)courseJSON.get(COURSE_DESCRIPTION);
+                UUID authorID = UUID.fromString((String)courseJSON.get(COURSE_AUTHOR_ID));
+                JSONArray studentsArray = (JSONArray)courseJSON.get(COURSE_STUDENTS_ARRAY);
+                for(int j=0;j<studentsArray.size();j++) {
+                    JSONObject studentJSON = (JSONObject)studentsArray.get(j);
+                    UUID studentID = UUID.fromString((String)studentJSON.get(COURSE_STUDENTS_ID));
+                    int completedTopics = (Integer)studentJSON.get(COURSE_STUDENT_GRADES);
+                    JSONArray gradesArray = (JSONArray)studentJSON.get(COURSE_STUDENT_GRADES);
+                    for(int k=0;k<gradesArray.size();k++) {
+                        //TODO grades
+                    }
+                    // set students grades
+                }
+                String difficulty = (String)courseJSON.get(COURSE_DIFFICULTY);
+                JSONArray topicsArray = (JSONArray)courseJSON.get(COURSE_TOPICS);
+                for(int j=0;j<topicsArray.size();j++) {
+                    JSONObject topicJSON = (JSONObject)topicsArray.get(j);
+                    String topicTitle = (String)topicJSON.get(COURSE_TOPIC_TITLE);
+                    JSONArray subtopicsArray = (JSONArray)topicJSON.get(COURSE_TOPIC_SUBTOPICS);
+                    for(int k=0;k<subtopicsArray.size();k++) {
+                        JSONObject subtopicJSON = (JSONObject)subtopicsArray.get(k);
+                        String subtopicTitle = (String)subtopicJSON.get(COURSE_TOPIC_SUBTOPICS_TITLE);
+                        String info = (String)subtopicJSON.get(COURSE_TOPIC_SUBTOPICS_INFO);
+                        subtopics.add(new Subtopic(subtopicTitle, info));
+                    }
+                    JSONArray quizArray = (JSONArray)topicJSON.get(COURSE_TOPIC_QUIZ);
+                    for(int k=0;k<quizArray.size();k++) {
+                        JSONObject quizJSON = (JSONObject)quizArray.get(k);
+                        String question = (String)quizJSON.get(COURSE_TOPIC_QUIZ_QUESTION);
+                        JSONArray answersArray = (JSONArray)quizJSON.get(COURSE_TOPIC_QUIZ_ANSWERS);
+                        String[] answers = new String[3];
+                        for(int y=0;y<answersArray.size();y++) {
+                            String answer = (String)answersArray.get(y);
+                            answers[y] = answer;
+                        }
+                        int correct = (Integer)quizJSON.get(COURSE_TOPIC_QUIZ_CORRECT);
+                        questions.add(new Question(question, answers, correct));
+                    }
+                    JSONArray commentsArray = (JSONArray)topicJSON.get(COURSE_TOPIC_COMMENTS);
+                    for(int k=0;k<commentsArray.size();k++) {
+                        JSONObject commentJSON = (JSONObject)commentsArray.get(k);
+                        String commentContent = (String)commentJSON.get(COURSE_TOPIC_COMMENTS_CONTENT);
+                        UUID creatorID = UUID.fromString((String)commentJSON.get(COURSE_TOPIC_COMMENTS_CREATOR_ID));
+                        JSONArray repliesArray = (JSONArray)commentJSON.get(COURSE_TOPIC_COMMENTS_REPLIES);
+                        ArrayList<Comment> replies = new ArrayList<>();
+                        for(int y=0;y<repliesArray.size();y++) {
+                            replies.add.getReplies((Comment)commentJSON.get(y));
+                        }
+                        comments.add(new Comment(creatorID, commentContent, "", replies));
+                    }
+                    topics.add(new Topic(subtopics, topicTitle, , new Quiz(questions)));
+                }
+                courses.add(new Course(id, topics, title, description, authorID));
+            }
+            reader.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return courses;
+    }
+
+    public static ArrayList<Comment> getReplies(Comment comment) {
+        ArrayList<Comment> replies = new ArrayList<>();
+        for(int i=0;i<replies.size();i++) {
+            replies.add(getReplies(replies.get(i)));
+        }
+        return replies;
+    }
+
     public static void main(String[] args) {
         /*ArrayList<User> test = getUsers();
         for(int i=0;i<test.size();i++) {
@@ -49,6 +135,8 @@ public class DataLoader extends DataConstants {
 
         UserList list = UserList.getInstanceUserList();
         ArrayList<User> users = list.getUsers();
+
+        
 
         for(User user : users){
             System.out.println(user.getUsername());
