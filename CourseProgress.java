93d1e8aea736de2a5d84aa256d420ff73ede17cc
Kuba Jerzmanowski <koobman.kj@gmail.com>
2023-03-19 16:39:04 -0400

    editing courseProg

--- CourseProgress.java
+++ CourseProgress.java
@@ -1,16 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class CourseProgress {
     
     private UUID id;
-    private HashMap<String, Double> quizzes;
+    private HashMap<String, Double> grades;
     private int numCompletedTopics;
     private double grade;
 
-    public CourseProgress(UUID id, HashMap<String, Double> quizzes, int numCompletedTopics, double grade) {
+    public CourseProgress(UUID id, HashMap<String, Double> grades, int numCompletedTopics, double grade) {
         this.id = id;
-        this.quizzes = quizzes;
+        this.grades = grades;
         this.grade = grade;
         this.numCompletedTopics = numCompletedTopics;
     }
