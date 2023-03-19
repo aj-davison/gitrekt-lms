31688b41f57eed113af36efeaf65e4e751da4bd3
Trevor Allison <trevorallison@me.com>
2023-03-19 16:06:55 -0400

    editing topic

--- Topic.java
+++ Topic.java
@@ -4,27 +4,21 @@ public class Topic {
     private ArrayList<Subtopic> subtopics;
     private ArrayList<Comment> comments; 
     private String name;
-    private Difficulty difficulty;
     private Quiz quiz;
-    private boolean complete;
     private int position = 0;
 
-    public Topic(ArrayList<Subtopic> subtopics, String name, Difficulty difficulty, Quiz quiz){
+    public Topic(ArrayList<Subtopic> subtopics, String name, Quiz quiz){
         this.subtopics = subtopics;
         this.name = name;
-        this.difficulty = difficulty;
         this.quiz = quiz;
         comments = new ArrayList<Comment>();
-        this.complete = false;
     }
 
     public Topic(ArrayList<Subtopic> subtopics, ArrayList<Comment> comments, String name, Difficulty difficulty, Quiz quiz, boolean isCompleted){
         this.subtopics = subtopics;
         this.comments = comments;
         this.name = name;
-        this.difficulty = difficulty;
         this.quiz = quiz;
-        this.complete = isCompleted;
     }
 
 
@@ -47,13 +41,12 @@ public class Topic {
     public boolean hasNext(){
         return subtopics.get(position) != null && position < subtopics.size();
     }
-    //TODO
+
     public int score(){
         return this.quiz.score();
     }
-    //TODO
     public String toString(){
-        return "";
+        return "Topic";
     }
     public void addComment(Comment comment){
         comments.add(comment);
