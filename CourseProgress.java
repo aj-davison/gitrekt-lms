b42ee4d5f3f79d02405238e98e1340a16997cec2
Trevor Allison <trevorallison@me.com>
2023-03-19 14:36:12 -0400

    changes to classes

--- CourseProgress.java
+++ CourseProgress.java
@@ -5,10 +5,12 @@ public class CourseProgress {
     private Course course;
     private HashMap<Quiz, Double> quizzes;
     private int numCompletedTopics;
+    private double grade;
 
-    public CourseProgress(Course course, HashMap<Quiz, Double> quizzes, int numCompletedTopics) {
+    public CourseProgress(Course course, HashMap<Quiz, Double> quizzes, int numCompletedTopics, double grade) {
         this.course = course;
         this.quizzes = quizzes;
+        this.grade = grade;
         this.numCompletedTopics = numCompletedTopics;
     }
     //TODO
@@ -16,8 +18,11 @@ public class CourseProgress {
         return "CourseProgress";
     }
 
-    public double calcGrade() {
+    public void calcGrade() {
         int questions = this.course.numCompletedTopics()*3;
-        return this.course.score()/questions;
+        this.grade = this.course.score()/questions;
+    }
+    public void setNumCompletedTopics(){
+        numCompletedTopics = this.course.numCompletedTopics();
     }
 }
