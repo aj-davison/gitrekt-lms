8cf3098b10fd8999f1188715e5c372e47e524c4a
Trevor Allison <trevorallison@me.com>
2023-03-23 12:16:58 -0400

    Cleaning up and adding numCompletedTopics Method

--- CourseProgress.java
+++ CourseProgress.java
@@ -2,30 +2,17 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class CourseProgress {
-    //private static CourseList courseList;
     private UUID id;
     private ArrayList<Double> grades;
-    private double grade;
-    //private int numCompletedTopics;
 
     public CourseProgress(UUID id, ArrayList<Double> grades) {
         this.id = id;
         this.grades = grades;
-        //this.numCompletedTopics = numCompletedTopics;
     }
-    
     //TODO
     public String toString() {
         return "CourseProgress";
     }
-    /* 
-    public void calcGrade() {
-        int questions = this.grades.size();
-        for(double score : grades) {
-            this.grade += score;
-        }
-        this.grade /= questions;
-    } */
     public void addGrade(double grade){
         this.grades.add(grade);
     }
@@ -41,12 +28,10 @@ public class CourseProgress {
     public ArrayList<Double> getGrades(){
         return this.grades;
     }
-
     public String getID() {
         return this.id.toString();
     }
-    /*
-    public void setNumCompletedTopics(){
-        //numCompletedTopics = this.course.numCompletedTopics();
-    } */
+    public int numCompletedTopics(){
+        return this.grades.size();
+    } 
 }
