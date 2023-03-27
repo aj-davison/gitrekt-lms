f5e54de2fc47a305a197d568a3a617b70d2a3dc2
Trevor Allison <trevorallison@me.com>
2023-03-27 15:10:16 -0400

    cleaning up signup in lms

--- LMS.java
+++ LMS.java
@@ -18,22 +18,7 @@ public class LMS {
         courseList.saveCourses();
     }
     public User signUp(String firstName, String lastName, String username, String password, String email, int type){
-        
-        UserList userList = UserList.getInstanceUserList();
-        if (type == 0) {
-            Student user = new Student(firstName, lastName, email, username, password);
-            userList.addStudent(firstName, lastName, email, username, password);
-            DataWriter.saveUsers();
-            return user;
-        } else if(type == 1){
-            Author user = new Author(firstName, lastName, email, username, password);
-            userList.addAuthor(firstName, lastName, email, username, password);
-            DataWriter.saveUsers();
-            return user;
-        } else {
-            System.out.println("Error, invalid input");
-            return null;
-        }
+        return userList.signUp(firstName, lastName, username, password, email, type);
     }
     public void enrollCourse(String title){
         CourseList courseList = CourseList.getInstanceCourseList();
