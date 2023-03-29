60998451720f5454bfa4cccd0687ea5ba6ae2739
Trevor Allison <trevorallison@me.com>
2023-03-28 22:47:52 -0400

    adding is completed

--- LMS.java
+++ LMS.java
@@ -78,6 +78,9 @@ public class LMS {
     public String currentCoursesToString() {
         return currentUser.currentCoursesToString();
     }
+    public boolean isCompleted(Course course){
+        return currentUser.getCourseProgress(course.getTitle()).isCompleted(course);
+    }
 
     public void addSubtopic(Topic topic, Subtopic subtopic){
         topic.addSubtopic(subtopic);
