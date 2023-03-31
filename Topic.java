3c4d2c27dc36b77f3a4086dd5e17ec36f44dddcd
Trevor Allison <trevorallison@me.com>
2023-03-30 22:59:53 -0400

    java doc topic

--- Topic.java
+++ Topic.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
-
+/**
+ * topic class
+ */
 public class Topic {
-    private ArrayList<Subtopic> subtopics;
-    private ArrayList<Comment> comments; 
-    private String name;
-    private Quiz quiz;
-    private int position;
+    private ArrayList<Subtopic> subtopics; //arraylist of subtopics
+    private ArrayList<Comment> comments; //arraylist of comments on topic
+    private String name; //name for topic
+    private Quiz quiz; //quiz for topic
+    private int position; //position for iterator
 
+    /**
+     * constructor for topic (new)
+     * @param subtopics
+     * @param name
+     * @param quiz
+     */
     public Topic(ArrayList<Subtopic> subtopics, String name, Quiz quiz){
         this.subtopics = subtopics;
         this.name = name;
@@ -14,7 +22,13 @@ public class Topic {
         comments = new ArrayList<Comment>();
         this.position = 0;
     }
-
+    /**
+     * contsructor for topic (loading)
+     * @param subtopics
+     * @param comments
+     * @param name
+     * @param quiz
+     */
     public Topic(ArrayList<Subtopic> subtopics, ArrayList<Comment> comments, String name,Quiz quiz){
         this.subtopics = subtopics;
         this.comments = comments;
@@ -22,7 +36,11 @@ public class Topic {
         this.quiz = quiz;
         this.position =0;
     }
-
+    /**
+     * equals method for topic
+     * @param topic
+     * @return true if this topic is equal to other topic
+     */
     public boolean equals(Topic topic){
         boolean result = false;
         boolean subtopicCheck = false;
@@ -46,6 +64,10 @@ public class Topic {
         }
         return result;
     }
+    /**
+     * test to see if topic is complete (uses iterator)
+     * @return true if topic is complete
+     */
     public boolean completionStatus(){
         boolean result;
         if(!hasNext()){
@@ -55,7 +77,10 @@ public class Topic {
         }
         return result;
     }
-
+    /**
+     * next subtopic method as part of iterator
+     * @return next subtopic if there is one
+     */
     public Subtopic nextSubtopic(){
         if(!hasNext()){
             return null;
@@ -64,19 +89,17 @@ public class Topic {
         position++;
         return subtopic;
     }
-
+    /**
+     * checks to see if topic has a next subtopic
+     * @return true if there are more subtopic(s) in the arraylist
+     */
     public boolean hasNext(){
         return subtopics.get(position) != null && position < subtopics.size();
     }
-    /* 
-    public String toString(){
-        String result = "";
-        result += this.name+"\n";
-        result += this.nextSubtopic().toString();
-        position++;
-        return result;
-    } */
-
+    /**
+     * displays string of all subtopics
+     * @return string of list of all subtopics
+     */
     public String displaySubtopics(){
         String result = "";
         int position = 1;
@@ -86,32 +109,52 @@ public class Topic {
         }
         return result;
     }
+    /**
+     * displays topics quiz
+     * @return string of topics quiz
+     */
     public String displayQuiz(){
         return this.quiz.toString();
     }
-
+    /**
+     * displays all info of topic
+     * @return string of subtopics and quiz for topic
+     */
     public String displayTopicInfo(){
         String result = this.displaySubtopics()+"\n";
         result += this.displayQuiz();
         return result;
     }
-
+    /**
+     * prints contents of topic to file (subtopics)
+     */
     public void printToFileTopic() {
         LMSFileWriter.topicToFile(this);
     }
-
+    /**
+     * adds comment to arraylist
+     * @param comment
+     */
     public void addComment(Comment comment){
         comments.add(comment);
     }
-
+    /**
+     * setter for quiz
+     * @param quiz
+     */
     public void addQuiz(Quiz quiz){
         this.quiz = quiz;
     }
-
+    /**
+     * adds subtopic to arraylist
+     * @param subtopic
+     */
     public void addSubtopic(Subtopic subtopic){
         subtopics.add(subtopic);
     }
-
+    /**
+     * @return returns arraylist of subtopics
+     */
     public ArrayList<Subtopic> getSubTop() {
         if(subtopics == null){
             subtopics = new ArrayList<Subtopic>();
@@ -120,15 +163,21 @@ public class Topic {
         else
             return this.subtopics;
     }
-
+    /**
+     * @return returns quiz for topic
+     */
     public Quiz getQuiz() {
         return this.quiz;
     }
-    
+    /**
+     * @return returns name for topic
+     */
     public String getName() {
         return this.name;
     }
-
+    /**
+     * @return returns arraylist of comments for topic
+     */
     public ArrayList<Comment> getComments() {
         if(comments == null) {
             comments = new ArrayList<Comment>();
