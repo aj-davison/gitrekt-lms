c80ccdfea4c2bc0a93e0241d8d57bbf2e81d83cb
chribar <chribar@email.sc.edu>
2023-04-06 12:22:53 -0400

    CourseListTest start

---
+++ tests/CourseListTest.java
@@ -0,0 +1,15 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+
+public class CourseListTest {
+
+    @Test
+    public void testing() {
+
+        assertTrue(true);
+    }
+    
+}
